2d54940a76d944785f6ba065ff498fbd62e729cc
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-03-14 15:07:05 -0400

    gg

--- day.js
+++ day.js
@@ -3,7 +3,7 @@ dataP.then(function(data)
 {
   console.log("data",data)
   //console.log("data",data[0].grades)
-  drawGraph(data,1000,600,0);
+  drawGraph(data,800,600,0);
 },
 function(err)
 {
@@ -18,12 +18,12 @@ var drawGraph=function(data,w,d,x)
     width:w,
     height:d
   };
-  var svg=d3.select("body").append("svg")
+  var svg=d3.select("svg")
             .attr("width",scren.width)
             .attr("height",scren.height)
   var margins=
   {
-    top:10,
+    top:20,
     bottom:50,
     left:35,
     right:70
@@ -49,13 +49,29 @@ var drawGraph=function(data,w,d,x)
                        .data(data[x].grades)
                        .enter()
                        .append("rect")
-                       .attr("fill",function(d){ return colors(d.name)})
+                       .attr("fill",function(d){ return colors(d.name);})
                        .attr("x",function(d,i){return xScale(i);})
                        .attr("y",function(d,i){return yScale(d.grade);})
                        .attr("width",width/4-4)
                        .attr("height",function(d){
                          return height-yScale(d.grade);
                        })
+                       .on("mouseover", function(d) {
+                          var xPosition = parseFloat(d3.select(this).attr("x")) + width/8;
+                          var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + height / 2;
+                         d3.select("#tooltip")
+                          .style("left", xPosition + "px")
+                          .style("top", yPosition + "px")
+                          .select("#value")
+                          .text(d.grade);
+                         d3.select("#tooltip").select("#name")
+                          .text(d.name);
+                          d3.select("#tooltip").select("#gradeday")
+                           .text(document.getElementById("day").innerText);
+
+                         d3.select("#tooltip").classed("hidden", false);})
+                        .on("mouseout", function() {
+                          d3.select("#tooltip").classed("hidden", true);});
   //the legend...
   var legend=svg.append("g")
                 .classed("legend",true)
@@ -86,42 +102,71 @@ var drawGraph=function(data,w,d,x)
   svg.append("g").classed("yAxis",true)
      .call(yAxis)
      .attr("transform","translate("+(margins.left-10)+","+(margins.top)+")");
+  document.getElementById("button1").disabled = true;
 };
 
-var updateGraph=function(data,x)
+var updateGraph=function(data,w,d,x)
 {
+
+  var scren=
+  {
+    width:w,
+    height:d
+  };
   var margins=
   {
-    top:10,
+    top:20,
     bottom:50,
     left:35,
     right:70
   }
-  var svg=d3.select("svg")
-  var width=svg.width-margins.left-margins.right;
-  var height=svg.height-margins.top-margins.bottom;
+  var svg=d3.select("svg");
+  var width=scren.width-margins.left-margins.right;
+  var height=scren.height-margins.top-margins.bottom;
+  var xScale=d3.scaleLinear()
+               .domain([0,4])
+               .range([0,width]);
   var yScale=d3.scaleLinear()
                .domain([0,100])
                .range([height,0]);
-  var students=svg.select("plotLand").selectAll("rect")
-                       .data(data[x].grades)
+  var colors=d3.scaleOrdinal(d3.schemeAccent);
+  var students=svg.selectAll("rect")
+                       .data(data[x-1].grades)
                        .transition()
-                       .attr("fill",function(d){ return colors(d.name)})
+                       .duration(1000)
+                       .attr("fill",function(d){ return colors(d.name);})
                        .attr("x",function(d,i){return xScale(i);})
                        .attr("y",function(d,i){return yScale(d.grade);})
                        .attr("width",width/4-4)
                        .attr("height",function(d){
                          return height-yScale(d.grade);
-                       })
+                       });
+
+
+  var day=document.getElementById("day");
+  day.innerText=data[x-1].day;
+  document.getElementById("button1").disabled = false;
+  document.getElementById("button2").disabled = false;
+  if(x==1)
+  {
+    document.getElementById("button1").disabled = true;
+  }
+  else if(x==10)
+  {
+    document.getElementById("button2").disabled = true;
+  }
 }
+
 
 var update=function(x)
 {
+  var y=document.getElementById("day").innerText;
+  y=Number(y)+Number(x);
   var dataP=d3.json("day.json");
   var day=d3.select("p");
   dataP.then(function(data)
   {
-    updateGraph(data,1);
+    updateGraph(data,800,600,y);
   },
   function(err)
   {
